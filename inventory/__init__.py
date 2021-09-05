ba733985420184f6ffc5c17aed105eaa4da55d3c
Davide Depau <davide@depau.eu>
2021-09-05 13:07:33 +0200

    Add external strings for makemessages

---
+++ inventory/__init__.py
@@ -0,0 +1,6 @@
+from django.utils.translation import ugettext_lazy as _
+
+# These are actually defined elsewhere but I want to provide custom translation
+# Placing them somewhere makes sure makemessages picks them up
+_("Log out")
+_("Parent")
